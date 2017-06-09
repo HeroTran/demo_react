410c1b57904701ea41184bbdc8790864a619fb7d
hung.tran <hung.tran@zinio.com>
2017-06-09 15:28:28 +0700

    update

--- app/components/Home/Home.js
+++ app/components/Home/Home.js
@@ -11,11 +11,17 @@ export default class Home extends React.Component {
       <div  className="Main-Section">
         <ul className="Section">
             <li className="Section-group">
-                <h2 className="Group-Title">List Site Magazine</h2>
+                <div className="Group-header">
+                    <h2 className="Group-Title">List Site Magazine</h2>
+                    <span className="Add-new"><i className="material-icons md-12 dark">add</i>
+                        Add Site
+                    </span>
+                </div>
+
                 <ul>
                     <li className="Section-issue">
                         <div className="Issue-cover">
-                            <img />
+                            <img src="../app/assets/images/default-img.png"/>
                             <div className="Issue-cover-overlay">
                                 <div className="Overlay-icons Issue-remove" >
                                     <i className="material-icons md-12 dark">delete</i>
